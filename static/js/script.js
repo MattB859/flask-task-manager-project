0c407b2e1bae69f3021a39207aabd19343e587fc
MattB859 <mateus756@hotmail.com>
2021-10-15 12:21:13 +0000

    Add Categories

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.sidenav').sidenav({inDuration: 400});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('select').formSelect();
     $('.datepicker').datepicker({
       format: "dd, mmmm, yyyy",
       yearRange: 3,
